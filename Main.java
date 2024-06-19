4b1f21ec92a42a757f8db957d78052a5440a031c
JENER-MILHOMEM <milhomemjener@gmail.com>
2024-06-19 17:22:10 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        double x1 = 4.00;
+        double x2 = 4.50;
+        double x3 = 5.00;
+        double x4 = 2.00;
+        double x5 = 1.50;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escolha algo do cardapio? ");
+        double a = sc.nextDouble();
+        System.out.println("quer algo mais? ");
+        double b = sc.nextDouble();
+
+
+       if(a == 1){
+           System.out.println(x1 );
+       }else if(a == 2){
+           System.out.println(x2 );
+       }else if(a == 3){
+           System.out.println(x3);
+       }else if(a == 4){
+           System.out.println(x4);
+       }else if(a == 5){
+           System.out.println(x5);
+       }
+
+    }
+    }
+
